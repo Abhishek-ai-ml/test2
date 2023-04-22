560343a4d12639db41382e9dbb4868a46bdbb5e6
Abhishek-ai-ml <abhishekdoulia@gmail.com>
2023-04-22 14:50:11 +0530

    working till audi selection

--- src/components/Seats.js
+++ src/components/Seats.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import audiData from '../utils/Audi';
 
 const Seats = (props) => {
-    let aud = props.aud;
+    let audId = props.aud;
 
     const [occupySeat, setOccupySeat] = useState(false);
 
@@ -11,30 +11,22 @@ const Seats = (props) => {
     }
 
     console.log('printing audi data in seats.js');
-    console.log(aud);
+    console.log(audId);
 
   return (
-    <div className='relative top-10'>
-      <div className='flex flex-wrap gap-x-5 mx-auto w-[500px] h-[400px]'>
-        {/* {
-            audi.seatInfo.map( (seat) => 
-                (
-                    !seat && <button className={`w-[50px] h-[50px] border-blue-900 border-2 rounded-md ${!occupySeat && !seat? 'bg-blue-900' : 'bg-white'}`} onClick={clickHandler}></button>
-                )
+    <div className='relative top-10 w-9/12 mx-auto h-screen'>
+      <div className={`flex flex-wrap gap-x-5 mx-auto w-9/12 h-[350px]  px-20 py-20 rounded-3xl shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px]`}>
+          {
+              audiData.map( (audi) => (
+                audi.id === audId && (
+                audi.seatInfo.map( (seat) => (
+                  !seat && <button className={`w-[50px] h-[50px] border-blue-900 border-2 rounded-md z-10 ${!occupySeat && !seat? 'bg-blue-900' : 'bg-white'}`} onClick={clickHandler}></button>
+                ))
+              )
+              )
+              
             )
-        } */}
-
-        {
-            audiData.map( (audi) => {
-            audi.id === aud && (
-            audi.seatInfo.map( (seat) => 
-                (
-                    !seat && <button className={`w-[50px] h-[50px] border-blue-900 border-2 rounded-md ${!occupySeat && !seat? 'bg-blue-900' : 'bg-white'}`} onClick={clickHandler}></button>
-                )
-            )
-            )
-          })
-        }
+          }
       </div>
     </div>
   )
